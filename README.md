3f906524747d97c1b97501f1be7f6fe4482dc100
Vinicius Cosmo <viniciuscosmosp@gmail.com>
2022-02-05 08:31:56 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Atualizando o input pelo movimento do mouse
+
+### Primeira versão
+<br />
+
+> Preview
+<br />
+<img width="100%" src="assets/video/prev.gif">
+
+<br />
+<p>
+Esse código consiste em permitir que o usuário atualize o preço usando o "Segurar e arrastar".
+</p>
+
+<br />
+<hr />
+
+## Ícones utilizados<br />
+
+### <a href="https://icofont.com/">Icofont</a>
+<ol>
+    <li>
+        icofont-arrow-left
+    </li>
+    <li>
+        icofont-arrow-right
+    </li>
+</ol>
---
+++ assets/functions/index.js
@@ -0,0 +1,69 @@
+const container = document.getElementById('input-container')
+const inputDisplay = container.querySelector('.display-price')
+const inputPrice = container.querySelector('.input-price')
+const inputStep = inputPrice.getAttribute('step')
+
+let mouseStatus = document.querySelector('#status span')
+let mouseDirection = document.querySelector('#direction span')
+
+let oldPageX
+
+const formatCurrency = (value) => {
+    const result = new Intl.NumberFormat('BRL', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(value)
+    return result
+}
+
+const updateInputDisplay = (value) => {
+    return inputDisplay.value = formatCurrency(value)
+}
+
+const changeInputValue = (direction) => {
+    inputPrice.value = Number(inputPrice.value) + (inputStep * direction)
+    updateInputDisplay(inputPrice.value)
+    return
+}
+
+container.addEventListener('mousemove', (evt) => {
+    if (!container.className.includes('focus')) return
+
+    let direction
+
+    if (evt.pageX < oldPageX) {
+        mouseDirection.innerText = 'LEFT'
+        direction = -1
+    }
+    else if (evt.pageX > oldPageX) {
+        mouseDirection.innerText = 'RIGHT'
+        direction = 1
+    }
+    else
+        return
+
+    oldPageX = evt.pageX
+
+    return changeInputValue(direction)
+})
+
+const loadEvents = () => {
+    container.addEventListener('mousedown', (evt) => {
+        oldPageX = evt.pageX
+        container.classList.add('mouse-focus')
+        mouseStatus.innerText = 'DOWN'
+    })
+
+    container.addEventListener('mouseup', () => {
+        container.classList.remove('mouse-focus')
+        mouseStatus.innerText = 'UP'
+    })
+
+    inputPrice.addEventListener('input', () => {
+        updateInputDisplay(inputPrice.value)
+    })
+
+    updateInputDisplay(inputPrice.value)
+}
+
+window.addEventListener('load', loadEvents)
